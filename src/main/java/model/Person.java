bef66c8072f85d5ff656d2ed586070824e485ae0
pgei <philipp.geiberger@stud.ubbcluj.ro>
2024-11-05 18:36:41 +0200

    Abstrakte Klasse Person inklusive Javadoc geschrieben, Check für Email-Adresse muss noch eingebaut werden

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,4 +1,36 @@
 package main.java.model;
 
-public class Person {
+/**
+ * Abstrakte Klasse, die grundlegende Attribute und Methoden für alle Nutzer der Anwendung definiert.
+ */
+public abstract class Person {
+
+    private String name;
+    private String email;
+    private String password;
+
+    /**
+     * Konstruktor für Erstellung eines Nutzers, wobei kontrolliert wird, dass die E-Mail-Adresse als Identifier einzigartig ist.
+     *
+     * @param name Name des Nutzers
+     * @param email E-Mail-Adresse des Nutzers, muss einzigartig sein
+     * @param password Passwort des Nutzers
+     */
+    public Person(String name, String email, String password) {
+        this.name = name;
+        //todo: check if email is unique
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * Methode die kontrolliert, ob ein gegebener String dem Passwort der Person entspricht.
+     * Vorgesehen zur Nutzung bei Anmeldung eines Nutzers, um die Authentizität zu überprüfen.
+     *
+     * @param password Das zu überprüfende Passwort
+     * @return Wahr falls gegebener String dem Passwort dieser Person entspricht, sonst falsch
+     */
+    public boolean isAuthentic(String password) {
+        return this.password.equals(password);
+    }
 }
