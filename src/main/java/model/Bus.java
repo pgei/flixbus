6cf7c2266b4a9633ee180047a782c8bae66822d5
pgei <philipp.geiberger@stud.ubbcluj.ro>
2024-11-10 23:04:09 +0200

    Klassen für Erweiterung von Transport für Zug und Bus geschrieben sowie kommentiert. Entsprechende Klassen für die Tickets erstellt.

---
+++ src/main/java/model/Bus.java
@@ -0,0 +1,47 @@
+package main.java.model;
+
+import java.util.HashMap;
+
+/**
+ * Klasse die Bustransporte repräsentiert, wobei die abstrakte Klasse Transport um Attribute und dazugehörige getter-Methoden erweitert wird.
+ */
+
+public class Bus extends Transport {
+
+    private int capacity;  //Kapazität die noch verfügbar ist (freie Plätze)
+    private HashMap<Integer, BusTicket> bookedSeats;
+
+    /**
+     * Konstuktor für Erstellung eines Bustransports, welcher den Konstuktor der erweiterten Klasse Transport überlädt sowie zusätzlich die anfängliche Kapazität des Buses definiert.
+     *
+     * @param origin        Ort an dem der Bustransport startet
+     * @param destination   Ort an dem der Bustransport endet
+     * @param year          Jahr in dem der Bustransport stattfindet
+     * @param month         Monat in dem der Bustransport stattfindet
+     * @param day           Datum des Tages an dem der Bustransport stattfindet
+     * @param hourd         Stunde, zu der der Bustransport startet
+     * @param mind          Minute, zu der der Bustransport startet
+     * @param houra         Stunde, zu der der Bustransport endet
+     * @param mina          Minute, zu der der Bustransport endet
+     * @param capacity      Gesamtkapazität des Bustransportes
+     */
+    public Bus(Location origin, Location destination, int year, int month, int day, int hourd, int mind, int houra, int mina, int capacity) {
+        super(origin, destination, year, month, day, hourd, mind, houra, mina);
+        this.capacity = capacity;
+    }
+
+    /**
+     * Getter für die zum Zeitpunkt des Methodenaufrufs noch verfügbare Kapazität
+     *
+     * @return Anzahl noch freier Sitzplätze auf dem Bustransport
+     */
+    public int getCapacity() {return this.capacity;}
+
+    /**
+     * Getter für HashMap welche alle, zum Zeitpunkt des Methodenaufrufs, auf dem Bustransport reservierten Tickets enthält
+     *
+     * @return HashMap mit allen gebuchten Tickets auf dem Bustransport
+     */
+    public HashMap<Integer, BusTicket> getBookedSeats() {return this.bookedSeats;}
+
+}
---
+++ src/main/java/model/Train.java
@@ -0,0 +1,57 @@
+package main.java.model;
+
+import java.util.HashMap;
+
+/**
+ * Klasse die Zugtransporte repräsentiert, wobei die abstrakte Klasse Transport um Attribute und dazugehörige getter-Methoden erweitert wird.
+ */
+
+public class Train extends Transport {
+
+    private int firstCapacity;  //Kapazität die noch verfügbar ist (freie Plätze 1. Klasse)
+    private int secondCapacity;  //Kapazität die noch verfügbar ist (freie Plätze 2. Klasse)
+    private HashMap<Integer, TrainTicket> bookedSeats;
+
+    /**
+     * Konstuktor für Erstellung eines Zugtransports, welcher den Konstuktor der erweiterten Klasse Transport überlädt sowie zusätzlich die anfänglichen Kapazitäten des Zuges in der 1. und 2. Klasse definiert.
+     *
+     * @param origin            Ort an dem der Zugtransport startet
+     * @param destination       Ort an dem der Zugtransport endet
+     * @param year              Jahr in dem der Zugtransport stattfindet
+     * @param month             Monat in dem der Zugtransport stattfindet
+     * @param day               Datum des Tages an dem der Zugtransport stattfindet
+     * @param hourd             Stunde, zu der der Zugtransport startet
+     * @param mind              Minute, zu der der Zugtransport startet
+     * @param houra             Stunde, zu der der Zugtransport endet
+     * @param mina              Minute, zu der der Zugtransport endet
+     * @param firstcapacity     Gesamtkapazität des Zugtransportes in der 1. Klasse
+     * @param secondcapacity    Gesamtkapazität des Zugtransportes in der 2. Klasse
+     */
+    public Train(Location origin, Location destination, int year, int month, int day, int hourd, int mind, int houra, int mina, int firstcapacity, int secondcapacity) {
+        super(origin, destination, year, month, day, hourd, mind, houra, mina);
+        this.firstCapacity = firstcapacity;
+        this.secondCapacity = secondcapacity;
+    }
+
+    /**
+     * Getter für die zum Zeitpunkt des Methodenaufrufs noch verfügbare Kapazität in der 1. Klasse
+     *
+     * @return Anzahl noch freier Sitzplätze in der 1. Klasse auf dem Zugtransport
+     */
+    public int getFirstCapacity() {return this.firstCapacity;}
+
+    /**
+     * Getter für die zum Zeitpunkt des Methodenaufrufs noch verfügbare Kapazität in der 2. Klasse
+     *
+     * @return Anzahl noch freier Sitzplätze in der 2. Klasse auf dem Zugtransport
+     */
+    public int getSecondCapacity() {return this.secondCapacity;}
+
+    /**
+     * Getter für HashMap welche alle, zum Zeitpunkt des Methodenaufrufs, auf dem Zugtransport reservierten Tickets enthält
+     *
+     * @return HashMap mit allen gebuchten Tickets auf dem Zugtransport
+     */
+    public HashMap<Integer, TrainTicket> getBookedSeats() {return this.bookedSeats;}
+
+}
