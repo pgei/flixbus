ea20e2f077911ea217707231cf1842aeaeccf36b
LupDani03 <daniel.lup@stud.ubbcluj.ro>
2024-11-12 14:33:55 +0200

    made the RequestHandler Class and implemented the 'start' method

--- src/main/java/controller/RequestHandler.java
+++ src/main/java/controller/RequestHandler.java
@@ -1,4 +1,52 @@
 package main.java.controller;
 
+import model.*;
+import service.BookingSystem;
+import java.util.Scanner;
+
 public class RequestHandler {
+    private Flixbus bookingSystem;
+    private Scanner scanner;
+
+    public  RequestHandler(Flixbus bookingSystem) {
+        this.bookingSystem = bookingSystem;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("Welcome to the Transport Booking System");
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nPlease select an option:");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. View Transports");
+            System.out.println("4. View Destinations");
+            System.out.println("5. Exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    register();
+                    break;
+                case 2:
+                    login();
+                    break;
+                case 3:
+                    viewTransports();
+                    break;
+                case 4:
+                    viewDestinations();
+                    break;
+                case 5:
+                    running = false;
+                    System.out.println("Exiting the system. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 }
